eae3696f4d20f4a1420ddc00dbc00a761cdef429
the_loraxxz <the_loraxxz@web.de>
2022-02-28 11:35:13 +0100

    Added dark mode

--- Frontend/src/app/screen/home/home-screen.component.ts
+++ Frontend/src/app/screen/home/home-screen.component.ts
@@ -9,10 +9,36 @@ export class HomeScreenComponent implements OnInit {
 
   constructor() { }
   currentDate:string = ""
+
   ngOnInit(): void {
     let date = new Date();
-    this.currentDate = date.getDate().toFixed(0);
-    this.currentDate.concat(date.getMonth().toFixed(0))
+    let tag = "";
+    tag=date.getDate().toString()+"."+date.getMonth().toString()+"."+date.getFullYear().toString()+" - "
+    switch (date.getDay()) {
+      case 1:
+        tag=tag+"Montag";
+        break;
+      case 2:
+        tag=tag+"Dienstag";
+        break;
+      case 3:
+        tag=tag+"Mittwoch";
+        break;
+      case 4:
+        tag=tag+"Donnerstag";
+        break;
+      case 5:
+        tag=tag+"Freitag";
+        break;
+      case 6:
+        tag=tag+"Samstag";
+        break;
+      case 7:
+        tag=tag+"Sonntag";
+        break;
+
+    }
+    this.currentDate = tag
   }
 
 }
